be4292b6725e70034042534b8e19d847acbe74ef
Arne <arne@Mac.fritz.box>
2026-01-27 23:29:10 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+
+export default function App() {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold">Bass Scales Trainer</h1>
+      <p>Core engine and fretboard UI coming in next iteration.</p>
+    </div>
+  )
+}
